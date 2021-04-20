cbd395a49deac8479121cc0a7486e0a7aaff3045
jeditt <jeditobiwan@outlook.com>
2021-04-19 22:58:10 -0400

    First attempt at keypress to slow time with a ramp back up to 1x. Requires an external tool such as IntructBot to hook into Twitch Chat

--- Deli.ExampleMod/src/ExampleAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Deli.ExampleMod
-{
-    // This is the format of our example asset. 
-    public struct ExampleAsset
-    {
-        public int Line;
-        public string Message;
-    }
-}
