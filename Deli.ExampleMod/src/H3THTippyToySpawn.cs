12b1a656b9c206f1959a3196a1176d2e0668ee48
jeditt <jeditobiwan@outlook.com>
2021-04-20 17:41:58 -0400

    separating scripts - am I even doing this correctly?

---
+++ Deli.ExampleMod/src/H3THTippyToySpawn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Deli.Immediate;
+using Deli.Setup;
+using Deli.VFS;
+using Deli.VFS.Disk;
+using UnityEngine.SceneManagement;
+using HarmonyLib;
+using UnityEngine;
+using Valve.VR;
+using FistVR;
+
+
+
+
+namespace TwitchSloMo
+{
+    // DeliBehaviours are just MonoBehaviours that get added to a global game object when the game first starts.
+    public class H3THTippyToySpawn : DeliBehaviour
+    {
+
+
+
+
+        private void Update()
+        {
+
+            if (Input.GetKeyDown("j"))
+
+            {
+                SpawnTippyToy();
+            }
+
+
+
+        }
+
+        private void SpawnTippyToy()
+        
+        {
+            Instantiate(FVRObject.)
+    
+
+        }
+
+        
+
+      
+            
+       
+    }
+}
