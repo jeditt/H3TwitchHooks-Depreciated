e83a7dc64bd2768e2d77542c7b39ab69b515dc40
jedit <jeditobiwan@gmail.com>
2021-04-20 23:01:44 -0400

    I am unable to grasp how to do this with one main script calling classes/functions from others, so I am keeping this simple for now. Consolidated to one script and was working to get both of the time slows working on keystroke, but they are fighting due to the scale up happening in DoSlowmotion.

--- H3TwitchHooks/src/Mod.cs
+++ H3TwitchHooks/src/Mod.cs
@@ -1,6 +1,7 @@
 ﻿using Deli.Setup;
 using UnityEngine;
 using Valve.VR;
+using System.Collections;
 
 namespace H3TwitchHooks 
 {
@@ -8,7 +9,7 @@ namespace H3TwitchHooks
     {
         //Keyword "const" means that it cannot be changed afterwards
         private const float SlowdownFactor = .001f;
-        private const float SlowdownLength = 6f;
+        private const float SlowdownLength = 5f;
 
 
         ///This is a constructor, it is called when a new instance of the object is being made! For example
@@ -30,11 +31,26 @@ namespace H3TwitchHooks
         private void Update()
         { 
             if (Input.GetKeyDown(KeyCode.Space)) //Use keycode here, less things can go wrong
-               DoSlowmotion();
+              { 
+         
+          //      This is to call the IEnumerator instead. This function currently fights with the StopTimeRandomly one
+          //      because I have the math to increase until 1 being called every frame, regardless of what calls it
+          //      StartCoroutine(BumpTime());
+
             
-            Time.timeScale += (1f / SlowdownLength) * Time.unscaledDeltaTime; 
-            Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency; 
-            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+            DoSlowmotion();
+            
+             Time.timeScale += (1f / SlowdownLength) * Time.unscaledDeltaTime; 
+             Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency; 
+             Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+               }
+
+             if (Input.GetKeyDown(KeyCode.H))
+
+            {
+                //this is currently not working, see DoSlowmotion comment
+                StartCoroutine(StopTimeRandomly());
+            }
         }
 
 
@@ -46,5 +62,30 @@ namespace H3TwitchHooks
             Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency;
             
         }
+
+        IEnumerator StopTimeRandomly()
+        {
+
+            // Freeze time
+            Time.timeScale = 0f;
+
+            // Pick a random duration from 3 to 10 seconds and wait for it to elapse 
+
+            float duration = UnityEngine.Random.Range(3, 10); yield return new WaitForSecondsRealtime(duration);
+
+            // Return time to normal 
+            Time.timeScale = 1f;
+        }
+
+    // The return math doesn't run at update here, so it stops after the first tick. Need to find a way to do this.
+    //    IEnumerator BumpTime()
+    //    {
+    //        Time.timeScale = SlowdownFactor;
+    //        Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency;
+    //        float bumpDuration = UnityEngine.Random.Range(1, 3); yield return new WaitForSecondsRealtime(bumpDuration);
+    //        Time.timeScale += (1f / SlowdownLength) * Time.unscaledDeltaTime; 
+    //        Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency; 
+    //        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    //    }
     }
 }
