84d60531ac09cb409af9a34f8bb11e50e7b2ccd5
Frityet <ambhogal01@gmail.com>
2021-05-02 10:22:33 -0700

    Fixed indentation

--- H3TwitchHooks/src/Mod.cs
+++ H3TwitchHooks/src/Mod.cs
@@ -8,16 +8,9 @@ namespace H3TwitchHooks
 {
     public class H3TwitchHooks : DeliBehaviour
     {
-        //Keyword "const" means that it cannot be changed afterwards
         private const float SlowdownFactor = .001f;
         private const float SlowdownLength = 6f;
-
-
-        ///This is a constructor, it is called when a new instance of the object is being made! For example
-        /// <code>
-        /// H3TwitchHooks obj = new H3TwitchHooks();
-        /// </code>
-        /// In the example when obj is made, the constructor of H3TwitchHooks is called
+        
         public H3TwitchHooks()
         {
             Logger.LogInfo("Loading H3TwitchHooks");
@@ -33,40 +26,31 @@ namespace H3TwitchHooks
 
         private void Update()
         { 
-            if (Input.GetKeyDown(KeyCode.Space)) //Use keycode here, less things can go wrong
-              {
-                    
-             DoSlowmotion();
-                
-                }
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                DoSlowmotion();
+            }
+            
             //return time to normal at a gradient
-             Time.timeScale += (1f / SlowdownLength) * Time.unscaledDeltaTime; 
-             Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency; 
-             Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+            Time.timeScale += (1f / SlowdownLength) * Time.unscaledDeltaTime; 
+            Time.fixedDeltaTime = Time.timeScale / SteamVR.instance.hmd_DisplayFrequency; 
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
               
 
             //wonderful toy spawn
-             if (Input.GetKeyDown(KeyCode.H))
-
+            if (Input.GetKeyDown(KeyCode.H))
             {
                 SpawnWonderfulToy();
-
-                
-               
             }
 
-             //body pillow spawn
-              if (Input.GetKeyDown(KeyCode.J))
-
+            //body pillow spawn
+            if (Input.GetKeyDown(KeyCode.J))
             {
                 SpawnPillow();
-
-                
-               
             }
 
-              //flash spawn
-              if (Input.GetKeyDown(KeyCode.K))
+            //flash spawn
+            if (Input.GetKeyDown(KeyCode.K))
             {
                 SpawnFlash();
             }
